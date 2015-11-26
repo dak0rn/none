d03f9b614f47679a3f04c91a0f063549d4be7bb7
dak0rn <daniel@suitsoft.eu>
2015-11-26 05:12:13 +0100

    Add first part of the build chain

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+/**
+ * Gruntfile.
+ * Build configuration for none.js
+ *
+ * Author:	Daniel Koch <daniel@suitsoft.eu>
+ * Creation:	26 Nov 2015
+ * Updated:	Time-stamp: <2015-11-26 05:09:33 dak0rn>
+ * Version:	1.0 - Initial release
+ *
+ */
+module.exports = function(grunt) {
+
+    require('load-grunt-config')(grunt, {
+
+        // Automatically execute grunt.initConfig()
+        init: true,
+
+        // Use jit-grunt to speed up the build process
+        jitGrunt: {
+
+            // Static mappings of file names to grunt plugins
+            staticMappings: {
+            }
+        }
+        
+    });
+    
+};
