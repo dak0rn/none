6c5c34431ec98ef86f24844db28ed363e67cf2c8
dak0rn <daniel@suitsoft.eu>
2015-11-26 06:20:17 +0100

    Exclude Emacs' Flycheck files

--- grunt/watch.js
+++ grunt/watch.js
@@ -3,7 +3,7 @@
  *
  * Author:	Daniel Koch <daniel@suitsoft.eu>
  * Creation:	26 Nov 2015
- * Updated:	Time-stamp: <2015-11-26 06:18:41 dak0rn>
+ * Updated:	Time-stamp: <2015-11-26 06:19:39 dak0rn>
  * Version:	1.0 - Initial release
  *
  */
@@ -11,12 +11,12 @@ module.exports = function() {
 
     return {
         none: {
-            files: ['src/**/*.js'],
+            files: ['src/**/*.js', '!src/**/flycheck*'],
             tasks: ['dev']
         },
 
         test: {
-            files: ['test/**/*.test.js'],
+            files: ['test/**/*.test.js', '!test/**/flycheck*'],
             tasks: ['test']
         }
     };
