cb49404228e09f0c0652d3fb89a48eea8f9ee1b5
dak0rn <daniel@suitsoft.eu>
2015-11-26 06:18:47 +0100

    Watch task for test development

--- grunt/watch.js
+++ grunt/watch.js
@@ -3,7 +3,7 @@
  *
  * Author:	Daniel Koch <daniel@suitsoft.eu>
  * Creation:	26 Nov 2015
- * Updated:	Time-stamp: <2015-11-26 05:23:14 dak0rn>
+ * Updated:	Time-stamp: <2015-11-26 06:18:41 dak0rn>
  * Version:	1.0 - Initial release
  *
  */
@@ -13,6 +13,11 @@ module.exports = function() {
         none: {
             files: ['src/**/*.js'],
             tasks: ['dev']
+        },
+
+        test: {
+            files: ['test/**/*.test.js'],
+            tasks: ['test']
         }
     };
 };
