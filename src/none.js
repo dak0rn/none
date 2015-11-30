c27e2fbdcb6feb05feefa880e8dc022193595082
dak0rn <daniel@suitsoft.eu>
2015-11-30 07:29:43 +0100

    Refactor comments for docco

--- src/none.js
+++ src/none.js
@@ -1,22 +1,15 @@
-/**
- * none.js.
- * Simple, flexible, beautiful.
- *
- * Author:	Daniel Koch <daniel@suitsoft.eu>
- * Creation:	26 Nov 2015
- * Updated:	Time-stamp: <2015-11-26 05:42:08 dak0rn>
- * Version:	1.0 - Initial release
- *
- */
+//     none.js.
+//     Simple, flexible, beautiful.
+//
+//     Author:     Daniel Koch <daniel@suitsoft.eu>
+//     Creation:   26 Nov 2015
+//     Updated:    Time-stamp: <2015-11-30 07:27:23 dak0rn>
+//     Version:    1.0 - Initial release
 
-/**
- * none.js main function.
- * Provides you with all the power and flexiblity of none.js
- * Supports currying and partial application and follows the
- * idea of a pure function.
- *
- *
- */
-//+ none :: () -> ()
+// none.js main function.
+//
+// Provides you with all the power and flexiblity of none.js
+// Supports currying and partial application and follows the
+// idea of a pure function.
 var none = function() {
 };
